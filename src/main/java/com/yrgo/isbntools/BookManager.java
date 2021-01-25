ca8a57636567e010232b7ba148362b7e36553053
jensbankel <jenban1204@skola.goteborg.se>
2021-01-25 12:31:09 +0100

    Corrected invalid code in BookManager

--- src/main/java/com/yrgo/isbntools/BookManager.java
+++ src/main/java/com/yrgo/isbntools/BookManager.java
@@ -10,7 +10,7 @@ public class BookManager {
 		List<Book> books = new ArrayList<Book>();
 		books.add(new Book("069117654X", "Understanding the Digital World","Brian Kernighan"));
 		books.add(new Book("0596009658","Learning the bash Shell","Cameron Newham"));
-		books.add(new Book("0123456789", "Java", "Bosse Boman"))
+		books.add(new Book("0123456789", "Java", "Bosse Boman"));
 		for(Book book:books) {
 			System.out.println(book);
 		}
